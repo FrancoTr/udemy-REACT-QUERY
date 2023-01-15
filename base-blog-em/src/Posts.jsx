a1fa66bf9453f3c6a039d11178e2e056e5ab274c
Franco Traverso <franco-t20@outlook.com>
2023-01-15 15:58:13 -0300

    Implement useQuery hook in order to fetch posts

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0");
   return response.json();
 }
 
@@ -14,23 +13,19 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data } = useQuery("posts", fetchPosts); //query key: 'posts' - fetchPosts needs to be asynchronous
+  if (!data) return <div>Waiting for data I guess?</div>;
 
   return (
     <>
       <ul>
         {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+          <li key={post.id} className='post-title' onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
+      <div className='pages'>
         <button disabled onClick={() => {}}>
           Previous page
         </button>
