b815294b53a8b008acd0aa5974b8f2a94c661f56
Franco Traverso <franco-t20@outlook.com>
2023-01-15 16:41:16 -0300

    Add staletime to useQuery

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -13,7 +13,7 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts); //query key: 'posts' - fetchPosts needs to be asynchronous
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, { staleTime: 2000 }); //query key: 'posts' - fetchPosts needs to be asynchronous
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
     return (
